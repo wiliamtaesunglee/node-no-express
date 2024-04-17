834d3fe43f9a0145f3803ddf91cb501c8cabfbf9
wiliam lee <wiliam.tae@gmail.com>
2024-04-17 08:01:50 -0300

    pass params to route

--- src/server.js
+++ src/server.js
@@ -8,13 +8,12 @@ const server = http.createServer(async (req, res) => {
   await json(req, res);
 
   const route = routes.find((route) => {
-    console.log(route.method, route.path.test(url));
     return route.method === method && route.path.test(url);
   });
 
   if (route) {
     const routeParams = req.url.match(route.path);
-    console.log({ routeParams });
+    req.params = { ...routeParams.groups };
     return route.handler(req, res);
   }
 
